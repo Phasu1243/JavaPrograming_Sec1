591e5e4739375aced1b61e2a30363ede0f6d8d5e
Phasu1243 <phasukeer@gmail.com>
2024-01-09 13:39:11 +0700

    JavaProject_Lab8

---
+++ JavaProject_Lab8/src/Circle.java
@@ -0,0 +1,16 @@
+/*OOP concept
+ *  class and Object */
+public class Circle {
+    //declare attribute name 'radius'
+	public double radius;
+	
+	//declare two methods in class Circle
+	public double getRadius() {
+		return radius;
+
+	}
+	public double getArea() {
+		return Math.PI* radius*radius;
+	}
+
+}
